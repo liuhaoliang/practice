39682d3893abec4327ba668b2188cfc811b8ef84
刘豪亮 <271072547@qq.com>
2019-10-25 09:07:42 +0800

    sss

--- log.py
+++ log.py
@@ -1,9 +1,22 @@
-#!/usr/bin/env python 
-# -*- coding:utf-8 -*- 
-import datetime 
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import datetime
+import os
+import threading
+
+path = './log.txt'
+
+
 def add(path, content):
-	with open(path,'a') as f: 
-		f.write(content + '\n') 
-path = './log.txt' 
-now_time = "{}".format(datetime.datetime.now()) 
-add(path, now_time)
+    with open(path, 'a') as f:
+        f.write(content + '\n')
+
+
+def fun_timer():
+    print('hello timer')
+    now_time = "{}".format(datetime.datetime.now())
+    add(path, now_time)
+    log = os.system(r"py log.py && git add . && git commit -m sss && git push")
+	print
+timer = threading.Timer(6, fun_timer)  # 首次启动
+timer.start()
